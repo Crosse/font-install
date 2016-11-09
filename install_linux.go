58684a0253bb218de13e3f41acf2ddeb67e8ae73
Seth Wright <seth@crosse.org>
2016-11-09 10:39:47 -0500

    Misplaced ) causes font-install to fail to build on Linux

--- install_linux.go
+++ install_linux.go
@@ -14,7 +14,9 @@ func platformDependentInstall(fontData *FontData) (err error) {
 	// font directory clean, install all font files for a particular font
 	// family into a subdirectory named after the family (with hyphens instead
 	// of spaces).
-	fullPath := path.Join(FontsDir, strings.ToLower(strings.Replace(fontData.Family, " ", "-", -1), fontData.FileName))
+	fullPath := path.Join(FontsDir,
+		strings.ToLower(strings.Replace(fontData.Family, " ", "-", -1)),
+		path.Base(fontData.FileName))
 	log.Debugf("Installing \"%v\" to %v", fontData.Name, fullPath)
 
 	if err = os.MkdirAll(path.Dir(fullPath), 0700); err != nil {
